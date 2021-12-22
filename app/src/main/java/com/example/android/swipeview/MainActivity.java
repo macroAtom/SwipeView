ecac17db6b94e574218efd6d76a16e6ed7247bed
macroAtom <1650556387@qq.com>
2021-12-22 15:32:06 +0800

    swipe view 周一到周五

---
+++ app/src/main/java/com/example/android/swipeview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.swipeview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+import android.os.Bundle;
+//import android.support.v4.view.ViewPager;
+//import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Displays a {@link ViewPager} where each page shows a different day of the week.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+    }
+}
